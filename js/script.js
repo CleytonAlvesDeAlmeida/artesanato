71647b81ab1f76be30c53bec5be279d92368f40c
Pedro W. <Pazevedodejesus@gmail.com>
2025-06-10 18:35:27 -0300

    Subindo em uma ramificação

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// // Atividade 1: Desestruturação de array de frutas
+// let frutas = ['Maçã', 'Banana', 'Laranja'];
+
+// const [ f1, f2, f3] = frutas;
+// console.log(`${f1}, ${f2}, ${f3}`);
+
+// Atividade 2: Desestruturação de objeto contendo informaçoes de um livro
+// let livro = {titulo: '1984', autor: 'George Orwell', ano: 1949};
+
+// const { titulo, autor, ano } = livro;
+// console.log(`O titulo é ${titulo}, o autor é ${autor} e o ano de lançamento é ${ano}`);
+
+// // Atividade 3: Desestruturação de array usando spread
+// let numeros = [1, 2, 3, 4, 5];
+
+
+// Atividade 4: Desestruturação de objeto
+// let pessoa = {nome: 'João', idade: 30};
+
+// const { nome, idade } = pessoa;
+// console.log(`${nome} tem ${idade} anos`);
+
+// // Atividade 5: Desestruturação de array dentro de função (Crie a função)
+// imprimir(['Olá', 'Mundo', '!']);
+
+
+// // Atividade 6: Desestruturação de objeto dentro de função (Crie a função)
+// imprimirPessoa({nome: 'Maria', idade: 25});
+
+// Atividade 7: Desestruturação de array aninhado
+// let matriz = [[1, 2], [3, 4]];
+
+// const [ p1, p2 ] = matriz;
+// const [ p, r ] = p1;
+// const [ d, t ] = p2;
+
+// console.log(p, r);
+// console.log(d, t);
+
+// Atividade 8: Desestruturação de objeto aninhado
+// let carro = {specs: {marca: 'Toyota', modelo: 'Corolla'}, ano: 2020};
+
+// const { specs, ano }  = carro;
+// const { marca, modelo} = specs;
+
+// console.log(`especificações: marca:${marca}, modelo:${modelo}, ano:${ano}`);
